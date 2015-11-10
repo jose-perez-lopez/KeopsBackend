dd2cfc80e9b0959451b2a13dbd02b77539b6b33b
Pepe <jose.perez@intelygenz.com>
2015-11-10 10:58:40 -0800

    added server port conf

--- src/server.js
+++ src/server.js
@@ -6,10 +6,18 @@ const server = restify.createServer();
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.post("/pyramids",require("./register_pyramids_endpoint"));
+function basicRespond(req, res, next) {
+    res.send('HELLO' );
+    next();
+}
+
+server.get("/",basicRespond);
+server.get("/pyramids",require("./get_pyramid_data_endpoint"));
 server.get("/pyramids/:pyramidId",require("./get_pyramid_data_endpoint"));
 server.put("/pyramids/:pyramidId",require("./set_pyramid_statuses_endpoint"));
+server.post("/pyramids",require("./register_pyramids_endpoint"));
 
+var serverPort = process.env.PORT || 3000;
 
 server.listen(3000,(error)=>{
     console.log('%s listening at %s', server.name, server.url);
