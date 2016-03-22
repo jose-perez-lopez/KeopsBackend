95910d7a585bae9638d7c7ece1b9b127bfa1ae0a
Jose Perez Lopez <jose.perez@intelygenz.com>
2016-03-21 17:35:37 -0700

    change http port environment variable to PORT

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ const config = fs.existsSync(process.cwd() + '/src/config.json') ? require('./co
 module.exports = {
 	redis_host: process.env.redis_host || config.redis_host,
 	redis_port: process.env.redis_port || config.redis_port,
-	http_port : process.env.http_port || config.http_port
+	http_port : process.env.PORT || config.http_port
 };
